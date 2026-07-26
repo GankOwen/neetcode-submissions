bf1cfdf4bafca747cf2f4e9f9bfe8f7d5ce58916
Wengan Ou <68822609+GankOwen@users.noreply.github.com>
2026-07-26 15:08:20 -0400

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for(int i = 2; i <= n; i++){
+            char c1 = s.charAt(i-1);
+            char c2 = s.charAt(i-2);
+
+            if(c1 != '0'){
+                dp[i] += dp[i-1];
+            }
+
+            int twoDigits = (c2 - '0') * 10 + (c1 - '0');
+            if(twoDigits >= 10 && twoDigits <= 26){
+                dp[i] += dp[i-2];
+            }
+            
+        }
+        return dp[n];
+    }
+}
