f88eed14d17e01c619251b1494b278c58ed63207
Wengan Ou <68822609+GankOwen@users.noreply.github.com>
2026-07-28 21:26:30 -0400

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return dfs(nums, 0, 0, target);
+    }
+    private int dfs(int[] nums, int index, int currentSum, int target){
+        if(index == nums.length){
+            return currentSum == target ? 1 : 0;
+        }
+
+        int add = dfs(nums, index+1, currentSum + nums[index], target);
+        int subtract = dfs(nums, index+1, currentSum - nums[index], target);
+
+        return add + subtract;
+    }
+}
