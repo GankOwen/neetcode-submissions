6a0485ea83b303d7ffd7b2debf992897a464bfbb
Wengan Ou <68822609+GankOwen@users.noreply.github.com>
2026-08-07 17:00:44 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.java
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size()==0)return true;
+        intervals.sort((a,b) -> Integer.compare(a.start, b.start));
+        Interval cur = intervals.get(0);
+        for(int i = 1; i < intervals.size(); i++){
+            Interval next = intervals.get(i);
+            if(cur.end > next.start) return false;
+            cur = next;
+        }
+        return true;
+    }
+}
